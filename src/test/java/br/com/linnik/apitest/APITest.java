86ec6aaf10282e8b7f08f31a45afb84359094abb
=igor_ufrn <=igor.ufrn@gmail.com>
2020-08-10 23:18:06 -0300

    first commit

---
+++ src/test/java/br/com/linnik/apitest/APITest.java
@@ -0,0 +1,69 @@
+package br.com.linnik.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured
+		.given()
+			.log().all()
+		.when()
+			.get("/todo")			
+		.then()
+			.log().all()
+			.statusCode(200)
+			
+		;
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		RestAssured
+		.given()
+			.log().all()
+			.contentType(ContentType.JSON)
+			.body("{\"task\":\"Teste via API\", \"dueDate\":\"2020-12-30\"}")			
+		.when()
+			.post("/todo")			
+		.then()
+			.log().all()
+			.statusCode(201)			
+		;
+	}
+	
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured
+		.given()
+			.log().all()
+			.contentType(ContentType.JSON)
+			.body("{\"task\":\"Teste via API\", \"dueDate\":\"2010-12-30\"}")			
+		.when()
+			.post("/todo")			
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+	
+	
+}
+
+
+
+
+
